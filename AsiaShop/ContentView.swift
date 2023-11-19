21fe2663aab275ad3e5ef100d34d35e4cf1d58fd
romangojshik <romangojshik2@gmail.com>
2023-11-19 17:29:15 +0300

    Add animation
    add animation between login and registration

--- AsiaShop/ContentView.swift
+++ AsiaShop/ContentView.swift
@@ -8,18 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isAuth = true
+    @State private var confirmPassword = ""
+    
     @State private var email = ""
     @State private var password = ""
     
     var body: some View {
         VStack(spacing: 20) {
-            Text("Авторизация")
+            Text(isAuth ? "Авторизация" : "Регистрация")
                 .bold()
-                .padding()
+                .padding(isAuth ? 15 : 35)
                 .padding(.horizontal, 30)
                 .font(.title2)
                 .background(Color("whiteAlpha"))
-                .cornerRadius(30)
+                .cornerRadius(isAuth ? 30 : 60)
             
             VStack {
                 TextField("Введите email", text: $email)
@@ -36,10 +39,23 @@ struct ContentView: View {
                     .padding(8)
                     .padding(.horizontal, 12)
                 
+                if !isAuth {
+                    SecureField("Повторите пароль", text: $confirmPassword)
+                        .padding()
+                        .background(Color("whiteAlpha"))
+                        .cornerRadius(12)
+                        .padding(8)
+                        .padding(.horizontal, 12)
+                }
+                
                 Button {
-                    print("Авторизация")
+                    if isAuth {
+                        print("Авторизация")
+                    } else {
+                        print("Регистрация")
+                    }
                 } label: {
-                    Text("Войти")
+                    Text(isAuth ? "Войти" : "Регистрация")
                         .bold()
                         .padding()
                         .frame(maxWidth: .infinity)
@@ -52,9 +68,9 @@ struct ContentView: View {
                 }
                 
                 Button {
-                    print("Регистрация")
+                    isAuth.toggle()
                 } label: {
-                    Text("Регистрация")
+                    Text(isAuth ? "Регистрация" : "Уже есть аккаунт!")
                         .bold()
                         .padding(.horizontal)
                         .frame(maxWidth: .infinity)
@@ -70,10 +86,15 @@ struct ContentView: View {
             .background(Color("whiteAlpha"))
             .cornerRadius(24)
             .padding()
-            .padding(30)
+            .padding(isAuth ? 30 : 15)
             
         }.frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Image("login_auth_v4")).ignoresSafeArea()
+        .background(
+            Image("login_auth_v4")
+                .ignoresSafeArea()
+                .blur(radius: isAuth ? 0 : 5)
+        )
+        .animation(Animation.easeOut(duration: 0.3), value: isAuth)
     }
 }
 
