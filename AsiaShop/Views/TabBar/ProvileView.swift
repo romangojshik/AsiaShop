fc8876fb1fa3ad58a40d6fa07e3924d5286d60e2
romangojshik <romangojshik2@gmail.com>
2023-11-27 00:53:24 +0300

    Profile

--- AsiaShop/Views/TabBar/ProvileView.swift
+++ AsiaShop/Views/TabBar/ProvileView.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct ProvileView: View {
+    @State var isAvatarAlertPresented = false
+    
     var body: some View {
-        Text("Профиль!")
+        VStack(alignment: .center, spacing: 20) {
+            HStack(spacing: 16) {
+                Image("profile")
+                    .resizable()
+                    .frame(width: 80, height: 80)
+                    .padding(.vertical)
+                    .clipShape(Circle())
+                    .onTapGesture {
+                        isAvatarAlertPresented.toggle()
+                    }
+                    .alcon
+                
+                VStack(alignment: .leading, spacing: 12) {
+                    Text("Имя Фамилия Отчество")
+                        .bold()
+                    Text("+375 29 111-11-11")
+                }
+            }
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Адресс доставки")
+                    .bold()
+                Text("Бреская область, город Брест")
+            }
+            
+            List {
+                Text("Ваши заказы будут тут!")
+            }.listStyle(PlainListStyle())
+            
+            Button {
+                Text("Выйти")
+            } label: {
+                Text("Выйти")
+                    .padding()
+                    .padding(.horizontal)
+                    .background(Color.red)
+                    .foregroundColor(Color.white)
+                    .cornerRadius(20)
+            }.padding()
+        }
     }
 }
 
