d0a565f21aaec5e397c1fb17ac7f5c5bd6fc0ef8
Roman Gojshik <romangojshik@MacBook-Pro-Roman.local>
2023-11-30 19:48:25 +0300

    Firebase
    create, add GoogleFirebase

--- AsiaShop/AsiaShopApp.swift
+++ AsiaShop/AsiaShopApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Firebase
 
 let screen = UIScreen.main.bounds
 
@@ -14,6 +15,16 @@ struct AsiaShopApp: App {
     var body: some Scene {
         WindowGroup {
             AuthView()
+        }
+    }
+    
+    class AppDelegate: NSObject, UIApplicationDelegate {
+        func application(
+            _ application: UIApplication,
+            didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+        ) -> Bool {
+            FirebaseApp.configure()
+            return true
         }
     }
 }
