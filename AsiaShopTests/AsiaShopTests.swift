a64b0bf01d380c4f40ca600a5b867be71896f35c
romangojshik2@gmail.com <romangojshik2@gmail.com>
2026-02-10 16:32:59 +0300

    add test target

---
+++ AsiaShopTests/AsiaShopTests.swift
@@ -0,0 +1,16 @@
+//
+//  AsiaShopTests.swift
+//  AsiaShopTests
+//
+//  Created by Roman Gojshik on 10.02.26.
+//
+
+import Testing
+
+struct AsiaShopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
